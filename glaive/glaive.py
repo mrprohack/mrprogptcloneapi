13e84a4bebdc1687ab12e613c2cc5f6fdf25b19b
mrprohack <kmani2782@gmail.com>
2023-09-29 15:25:54 +0000

    add __init__.py file

---
+++ glaive/glaive.py
@@ -0,0 +1,51 @@
+import requests
+
+
+url = 'https://arena.glaive.ai/generate'
+
+headers = {
+    'authority': 'arena.glaive.ai',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.5',
+    'content-type': 'application/json',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+}
+
+
+def gpt(prompt, model="GPT-3.5", maxTokens=1024, temperature=0.1):
+    allowed_models = ["WizardCoder-Python-34B-V1.0", "GPT-4", "GPT-3.5",
+                      "Phind-CodeLlama-34B-v2", "codellama-13b-oasst-sft-v10",
+                      "glaive-coder-7b"]
+
+    if model not in allowed_models:
+        return ValueError(f"Invalid model '{model}'. Please choose from: \
+        {', '.join(allowed_models)}")
+
+    data = {
+        "model": model,
+        "messages": [
+            {
+                "content": prompt,
+                "id": "",
+                "role": "user"
+            }
+        ],
+        "title": "New chat",
+        "id": None,
+        "maxTokens": maxTokens,
+        "temperature": temperature,
+        "topP": 0.95,
+        "topK": 30
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code != 200:
+        return "server error"
+    response_json = response.json()
+    return response_json['messages'][1]['content']
+
+
+if __name__ == "__main__":
+    print(gpt(
+        "how are you and what are you doing", model="GPT-3.5"))
---
+++ main.py
@@ -0,0 +1,3 @@
+from glaive import glaive
+
+print(glaive.gpt("how are you and what are you doing", model="GPT-3.5"))
